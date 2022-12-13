b1ee948b8a93167a9249759c07ccd99148b5faa0
timjb96 <timjeromebrown@gmail.com>
2022-12-12 16:35:04 -0800

    url update

--- Simple Map/static/js/logic.js
+++ Simple Map/static/js/logic.js
@@ -6,7 +6,7 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
   // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
